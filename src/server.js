79f26502fa20c5d0b15adcf632201694230c4d2a
parkGaYeon <gay0830@naver.com>
2023-12-12 23:13:03 +0900

    feature : webSocket 추가

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,8 +7,13 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+// 하나의 서버에서 http, webSocket 서버를 둘다 돌릴 수 있게 함. 
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+server.listen(3000, handleListen);
