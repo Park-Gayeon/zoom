0142678c46b6633bddb346d356ace7de366d56c4
parkGaYeon <gay0830@naver.com>
2023-12-17 22:58:56 +0900

    feature : WebSocket Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,19 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// socket이 connection을 open 하면 발생하는 log
+socket.addEventListener("open", () => {
+    console.log("Connected to Server");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ");
+});
+
+// 10초 뒤에 작동
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
